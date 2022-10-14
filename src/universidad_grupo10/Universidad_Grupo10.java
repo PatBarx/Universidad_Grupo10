6ac971354816a70672aa3738cfd3eae37cfbbc27
PatBarx <112406838+PatBarx@users.noreply.github.com>
2022-10-14 17:22:02 -0300

    Initial commit

---
+++ src/universidad_grupo10/Universidad_Grupo10.java
@@ -0,0 +1,12 @@
+package universidad_grupo10;
+
+/**
+ * @author Grupo 10
+ */
+public class Universidad_Grupo10 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
