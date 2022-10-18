9669c9f89e5344ecf1f0964e837ac4bad680ccbf
PatBarx <112406838+PatBarx@users.noreply.github.com>
2022-10-18 09:26:57 -0300

    aclaracion para caso de mysql
    com.mysql.jdbc.Driver

--- src/Data/MiConexion.java
+++ src/Data/MiConexion.java
@@ -25,7 +25,7 @@ public class MiConexion {
     public Connection buscarConexion() {
         if (conexion == null) {  //si es la primera vez q se usa la vamos a establecer
             try {
-                Class.forName("org.mariadb.jdbc.Driver");
+                Class.forName("org.mariadb.jdbc.Driver");   //com.mysql.jdbc.Driver --> cuando usemos mysql
                 conexion = DriverManager.getConnection(url, usuario , pass);
             } catch (SQLException |ClassNotFoundException ex) { //si no deja poner SQLException | delante de la Class..excepcion es porque falta la linea de arriba
                 Logger.getLogger(MiConexion.class.getName()).log(Level.SEVERE, null, ex);
